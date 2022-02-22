6b08f7cd3a303cffe7df845687441a511dea231e
hafidzencis <hafidzfebrian21@gmail.com>
2022-02-22 14:35:39 +0700

    adding point 4

--- 2_Version Control and Branch Management (Git)/praktikum/src/com/Master.java
+++ 2_Version Control and Branch Management (Git)/praktikum/src/com/Master.java
@@ -4,5 +4,6 @@ public class Master {
     public static void main(String[] args) {
         System.out.println("point-1");
         System.out.println("point-2");
+        System.out.println("point-4");
     }
 }
